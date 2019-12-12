baa30005fe863f533825be1a554cbbb21ff9817e
David Battye <david2.battye@live.uwe.ac.uk>
2019-12-12 18:32:44 +0000

    Add ability to login route (test using postman) in auth routes/api, mostly taken from users route/api. Previous commit was ability to register , this one login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -15,6 +19,62 @@ router.get('/', auth, async (req, res) => {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
+});
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token (login)
+// @access  Public
+router.post(
+  '/', 
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) { // if there are errors
+      return res.status(400).json({ errors }); // send 400 and send back json error message
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if(!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password); // bcrypt has a method called compare
+
+      if(!isMatch) {
+        return res
+        .status(400)
+        .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+      // Return jsonwebtoken
+      const payload = { // payload is the second part of the jwt
+        user: {
+          id: user.id // mongodb uses _id, mongoose uses abstraction
+        }
+      };
+
+      jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {// the callback function
+        if(err) throw err;
+        res.json({ token });
+      });
+      
+    } catch(err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
 });
 
 module.exports = router;
