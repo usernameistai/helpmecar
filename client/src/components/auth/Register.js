9fecd1d299f5b9b87f30915ca47ada0f7081706e
David Battye <david2.battye@live.uwe.ac.uk>
2020-01-13 17:31:31 +0000

    auth reducer and register action, need to relook at this

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => { // destructured, pulled out of props
+const Register = ({ setAlert, regsiter }) => { // destructured, pulled out of props
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -24,7 +25,7 @@ const Register = ({ setAlert }) => { // destructured, pulled out of props
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('Success');
+      register({ name, email, password });
     }
   };
 
@@ -40,7 +41,6 @@ const Register = ({ setAlert }) => { // destructured, pulled out of props
             name='name' 
             value={name}
             onChange={e => onChange(e)}
-            required 
           />
         </div>
         <div className='form-group'>
@@ -50,7 +50,6 @@ const Register = ({ setAlert }) => { // destructured, pulled out of props
             name='email'
             value={email}
             onChange={e => onChange(e)}
-            required
           />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
@@ -87,7 +86,8 @@ const Register = ({ setAlert }) => { // destructured, pulled out of props
 }
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
