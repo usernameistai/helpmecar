a0443e20f07ae190b4cd9214963c8912077e1a77
David Battye <david2.battye@live.uwe.ac.uk>
2020-01-14 03:34:27 +0000

    add user login actions

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert, regsiter }) => { // destructured, pulled out of props
+const Register = ({ setAlert, register, isAuthenticated }) => { // destructured, pulled out of props
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -28,6 +28,10 @@ const Register = ({ setAlert, regsiter }) => { // destructured, pulled out of pr
       register({ name, email, password });
     }
   };
+
+  if(isAuthenticated) {
+    return <Redirect to='/dashboard' />
+  }
 
   return (
     <Fragment>
@@ -87,7 +91,12 @@ const Register = ({ setAlert, regsiter }) => { // destructured, pulled out of pr
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
-export default connect(null, { setAlert, register })(Register);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
