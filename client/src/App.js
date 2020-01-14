c131b505597dee39385eaaae41d44ffa8a20cf43
David Battye <david2.battye@live.uwe.ac.uk>
2020-01-14 02:29:06 +0000

    add load user action and set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -8,9 +8,20 @@ import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux'; // connects the two
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
+
 import './App.css';
 
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []); // extra brackets means it only runs once, like componentDidMount
+
   return (
     <Provider store={store}>
       <Router>
