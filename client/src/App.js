be7c3b00076c3a0b669f8ced0952f02e9f18778d
David Battye <david2.battye@live.uwe.ac.uk>
2020-01-19 17:24:08 +0000

    add dashboard and privateroute auth component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux'; // connects the two
 import store from './store';
@@ -31,8 +33,9 @@ const App = () => {
           <section className="container">
             <Alert />
             <Switch>
-              <Route exact path='/register' component={ Register } />
-              <Route exact path='/login' component={ Login } />
+              <PrivateRoute exact path='/register' component={ Register } />
+              <PrivateRoute exact path='/login' component={ Login } />
+              <PrivateRoute exact path='/dashboard' component={ Dashboard } />
             </Switch>
           </section>
         </Fragment>
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,34 @@
+// This is a good way of having routes that require logins
+
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+  // the rest operator gets any other parameters that are passed in
+const PrivateRoute = ({ 
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest 
+}) => (
+  <Route 
+    {...rest} 
+    render={props => 
+      !isAuthenticated && !loading ? (
+        <Redirect to='/login' />
+      ) : (
+        <Component {...props} /* any props passed into it *//>
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth // pull in all the state in the auth reducer
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
