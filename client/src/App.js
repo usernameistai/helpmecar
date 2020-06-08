c814e2da379be006d5709058b13e2ceb5b7c9dc4
David Battye <david2.battye@live.uwe.ac.uk>
2020-06-08 22:21:08 +0100

    not found not working so well, remove

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import ScrollToTop from './ScrollToTop';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-import Routes from './components/routing/Routes';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+import Reg from './components/reg/Reg';
+import RegForm from './components/reg/RegForm';
+import SearchReg from './components/reg/SearchReg';
+import ShowReg from './components/reg/ShowReg';
+import RegRules from './components/reg/RegRules';
+import RegHelp from './components/reg/RegHelp';
+import RegSafety from './components/reg/RegSafety';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 import { Provider } from 'react-redux'; // connects the two
@@ -28,10 +47,31 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Switch >
-            <Route exact={true} path='/' component={ Landing } /> {/* added exact={true} */}
-            <Route component={ Routes} />
-          </Switch>
+          <Route exact={true} path='/' component={ Landing } /> {/* added exact={true} */}
+          <section className='container'>
+            <Alert />
+            <ScrollToTop />
+            <Switch>
+              <Route exact path='/register' component={ Register } />
+              <Route exact path='/login' component={ Login } />
+              <Route exact path='/profiles' component={ Profiles } />
+              <Route exact path='/profile/:id' component={ Profile } />
+              <PrivateRoute exact path='/dashboard' component={ Dashboard } />
+              <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
+              <PrivateRoute exact path='/edit-profile' component={ EditProfile } />
+              <PrivateRoute exact path='/add-experience' component={ AddExperience } />
+              <PrivateRoute exact path='/add-education' component={ AddEducation } />
+              <PrivateRoute exact path='/posts' component={ Posts } />
+              <PrivateRoute exact path='/posts/:id' component={ Post } />
+              <Route exact path='/reg' component={ Reg } />
+              <Route exact path='/reg-form' component={ RegForm } />
+              <Route exact path='/reg-search' component={ SearchReg } />
+              <Route exact path='/reg-search/:id' component={ ShowReg } />
+              <Route exact path='/reg-rules' component={ RegRules } />
+              <Route exact path='/reg-help' component={ RegHelp } />
+              <Route exact path='/reg-safety' component={ RegSafety } />
+            </Switch>
+          </section>
           <Footer />
         </Fragment>
       </Router>
