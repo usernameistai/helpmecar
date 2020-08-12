2f647cacfc406a962cabfec630138b66f50a6747
David Battye <david2.battye@live.uwe.ac.uk>
2020-08-12 22:06:51 +0100

    change button style to match rest in adeducation experience

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -110,7 +110,7 @@ const AddExperience = ({ addExperience, history }) => {
             onChange={e => onChange(e)}
           />
         </div>
-        <input type='submit' className='btn btn-primary my-1' />
+        <input type='submit' className='btn btn-primary my-1 noapp' />
         <Link className='btn btn-light my-1' to='/dashboard'>
           Go Back
         </Link>
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -216,7 +216,7 @@ const CreateProfile = ({
 					</Fragment>
 				)}
 
-				<input type='submit' className='btn btn-primary my-1' />
+				<input type='submit' className='btn btn-primary my-1 noapp' />
 				<Link className='btn btn-light my-1' to='/dashboard'>
 					Go Back
 				</Link>
